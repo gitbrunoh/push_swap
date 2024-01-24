1df608960fe554cea1e03b51b1605d41b9bf5d52
Bruno Lopes <brunohenriqueufpr@gmail.com>
2024-01-24 16:13:59 +0000

    naming errors4

--- utils.c
+++ utils.c
@@ -30,7 +30,7 @@ bool	stack_sorted(t_stack_node *stack) //Define a function that checks if the st
 		return (1);
 	while (stack->next) //Loop until the end of the stack is reached
 	{
-		if (stack->n > stack->next->nbr) //Check if the current value is larger than the next node's value, indicating it is out of sort
+		if (stack->n > stack->next->n) //Check if the current value is larger than the next node's value, indicating it is out of sort
 			return (false);
 		stack = stack->next; //If not, move to the next node for processing
 	}
