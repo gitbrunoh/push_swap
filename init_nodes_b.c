aa9a3d4f478d590958ab7334c595fe7e903d76f8
Bruno Lopes <brunohenriqueufpr@gmail.com>
2024-01-25 18:37:43 +0000

    avanço bom

--- init_nodes_b.c
+++ init_nodes_b.c
@@ -1,34 +1,34 @@
 #include "push_swap.h"
 
-static void	set_target_b(t_stack_node *a, t_stack_node *b) //Definir os targets para os nós do stack `b`
+static void	set_target_b(t_stack_node *a, t_stack_node *b)
 {
 	t_stack_node	*current_a;
 	t_stack_node	*target_node;
-	long			best_match_index;
+	long			closest_bigger;
 
 	while (b)
 	{
-		best_match_index = LONG_MAX;
+		closest_bigger = LONG_MAX;
 		current_a = a;
 		while (current_a)
 		{
 			if (current_a->n > b->n 
-				&& current_a->n < best_match_index) //Check if the `a` node's value is bigger than `b` node, && smaller than the "closest bigger" so far
+				&& current_a->n < closest_bigger)
 			{
-				best_match_index = current_a->n; //Set the best match as the value in the current `a` node
-				target_node = current_a; //Set `b` node's target node pointer to point to the current `a` node
+				closest_bigger = current_a->n;
+				target_node = current_a;
 			}
-			current_a = current_a->next; //Move to the next `a` node for processing
+			current_a = current_a->next;
 		}
-		if (best_match_index == LONG_MAX) //If the best match value has not changed
-			b->target = find_min(a); //Set `b` node's target node pointer to point to the smallest number in stack `a`
+		if (closest_bigger == LONG_MAX)
+			b->target = find_min(a);
 		else
-			b->target = target_node; //If the best match value has been updated, then we have a target node for the current `b` node
-		b = b->next; //Move to the next `b` node for processing
+			b->target = target_node;
+		b = b->next;
 	}
 }
 
-void	init_nodes_b(t_stack_node *a, t_stack_node *b) //Define a function that prepares the nodes for pushing `b` to `a`
+void	init_nodes_b(t_stack_node *a, t_stack_node *b)
 {
 	set_position(a);
 	set_position(b);
--- init_stack_a.c
+++ init_stack_a.c
@@ -6,7 +6,7 @@ static long	ft_atol(const char *s)
 	int		sign;
 
 	result = 0;
-	sign = 1; 
+	sign = 1;
 	while (*s == ' ' || *s == '\t' || *s == '\n' || \
 			*s == '\r' || *s == '\f' || *s == '\v')
 		s++;
@@ -21,33 +21,33 @@ static long	ft_atol(const char *s)
 	return (result * sign);
 }
 
-static void	append_node(t_stack_node **stack, int n) //Define a function that searches for the last node to append to the linked list
+static void	append_node(t_stack_node **stack, int n)
 {
-	t_stack_node	*node; //To store a pointer to the new node to be created with the value `n`
-	t_stack_node	*last_node; //To store a pointer to the current last node of the stack
+	t_stack_node	*new;
+	t_stack_node	*last_node;
 
 	if (!stack)
 		return ;
-	node = malloc(sizeof(t_stack_node)); //Allocate memory for the new node
-	if (!node)
+	new = malloc(sizeof(t_stack_node));
+	if (!new)
 		return ;
-	node->cheapest = false;
-	node->next = NULL; //Set the next pointer of the new node to NULL because it will be the last node in the list
-	node->n = n; //Set the `next` data of of the new node to `n` value
-	if (!(*stack)) //Check if the stack is empty or currently pointing to NULL, indicating a first node needs to be found
+	new->cheapest = false;
+	new->next = NULL; //será o último node da pilha, ou seja, o lá de baixo
+	new->n = n;
+	if (!(*stack))
 	{
-		*stack = node; //If empty, update the pointer *stack to point to the node, effectively making it the new head of the linked list
-		node->previous = NULL; //Set the head node's previous pointer to NULL as it's the first node
+		*stack = new;
+		new->previous = NULL; //O parâmetro nexta já está NULL, mas o previous não
 	}
-	else //If the stack is not empty, it means there are existing nodes in the linked list
+	else
 	{
-		last_node = find_last(*stack); //In which case, find the last node
-		last_node->next = node; //Append the new node to the last node
-		node->previous = last_node; //Update the previous pointer of the new node and complete the appending
+		last_node = find_last(*stack);
+		last_node->next = new;
+		new->previous = last_node;
 	}
 }
 
-void	init_stack_a(t_stack_node **a, char **argv) 
+void	init_stack_a(t_stack_node **a, char **argv)
 {
 	long	n;
 	int		i;
@@ -61,7 +61,7 @@ void	init_stack_a(t_stack_node **a, char **argv)
 		if (n > INT_MAX || n < INT_MIN)
 			free_errors(a);
 		if (error_duplicate(*a, (int)n))
-			free_errors(a); 
+			free_errors(a);
 		append_node(a, (int)n);
 		i++;
 	}
