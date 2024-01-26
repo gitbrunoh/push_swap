1fc3b201da81667662246976ac6ec0eee41c8983
Bruno Lopes <brunohenriqueufpr@gmail.com>
2024-01-26 14:27:09 +0000

    normal

--- push_operation.c
+++ push_operation.c
@@ -1,37 +1,37 @@
 #include "push_swap.h"
 
-static void	push(t_stack_node **dst, t_stack_node **src) //Define a function that pushes a top node, from one stack to another's top node
+static void	push(t_stack_node **dst, t_stack_node **src)
 {
-	t_stack_node	*push_node; //Used to store the pointer to the node to be pushed
+	t_stack_node	*push_node; //Armazenar o node que será pushado
 
-	if (!*src) //The top node of a stack to be pushed
+	if (!*src) //O top node que será pushado existe?
 		return ;
-	push_node = *src; //The top node to push is assigned to the `t_stack_node` variable
-	*src = (*src)->next; //Move the pointer of the stack to the next node, which will become the next `top node` after the node before is "popped off"
-	if (*src) //Check if the current node exists
-		(*src)->previous = NULL; //Set the current node as the head of the stack
+	push_node = *src;
+	*src = (*src)->next; 
+	if (*src) //Checar se essse next node existe
+		(*src)->previous = NULL; //Se sim, ele passa a ser o head, ou seja, tem o previous como NULL
 	push_node->previous = NULL; //Detach the node to push from its stack
-	if (!*dst) //Check if the other stack is empty
+	if (!*dst)
 	{
-		*dst = push_node; //If it's empty, assign as the first node of that stack, the node we want pushed
-		push_node->next = NULL; //Ensure it is also set as the last node, e.g. properly null terminate the stack
+		*dst = push_node; //Se o outro stack estiver vazio, o node que será pushado passa a ser o head
+		push_node->next = NULL; //Assegurar que é o último node da pilha e terminar com NULL
 	}
-	else //If the other stack we want to push to is not empty
+	else  
 	{
-		push_node->next = *dst; //Assign the node to push, to the top of the current top node of the stack
-		push_node->next->previous = push_node; //Assign to the "second node" `prev` attribute, the pushed node as the current top node
-		*dst = push_node; //Complete appending the node. The pointer to the top node of the stack is now pointing to our recently pushed node
+		push_node->next = *dst; //o Next do novo head de b será o dst, ou seja, o head antigo
+		push_node->next->previous = push_node; //Se o next é o head antigo, o previous dele é o novo head
+		*dst = push_node; //Agora sim, dando o correto nome aos bois. O push_node é o novo head
 	}
 }
 
-void	pa(t_stack_node **a, t_stack_node **b, bool write_flag) //Push on top of `b`, the top `a` and print the instruction
+void	pa(t_stack_node **a, t_stack_node **b, bool write_flag)
 {
 	push(a, b); 
 	if (!write_flag) 
 		write(1, "pa\n", 3);
 }
 
-void	pb(t_stack_node **b, t_stack_node **a, bool write_flag) //Push on top of `a`, the top `b` and print the instruction
+void	pb(t_stack_node **b, t_stack_node **a, bool write_flag)
 {
 	push(b, a);
 	if (!write_flag)
