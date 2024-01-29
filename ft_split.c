d036ee3866308394e2bedd766406ac7d40174e67
Bruno Lopes <brunohenriqueufpr@gmail.com>
2024-01-29 15:30:55 +0000

    teste com outra split entendendo o a questão do argv + 1 e sintetizando para ter menos de 25 linhas com a sugestão do copilot

--- ft_split.c
+++ ft_split.c
@@ -34,12 +34,49 @@ static char	*ft_strcopy(char *str1, char *str2, int n)
 	return (str1);
 }
          
-char    **ft_splitpush(char *str)
+static char **allocate_memory(char **out, int k, int size)
 {
-	int	i = 0;
-	int	j = 0;
-	int	k = 0;
-	char	**out;
+	out[k] = (char *)malloc(sizeof(char) * size);
+	if (out[k] == NULL)
+	{
+		while (k >= 0)
+			free(out[k--]);
+		free(out);
+		return (NULL);
+	}
+	return out;
+}
+
+static char **handle_token(char **out, char *str, int *i, int *k)
+{
+	int j = *i;
+	while (str[*i] != '\0' && str[*i] != ' ')
+		(*i)++;
+	if (*i > j)
+	{
+		out = allocate_memory(out, *k, (*i - j) + 1);
+		if (out == NULL)
+			return (NULL);
+		ft_strcopy(out[*k], str + j, *i - j);
+		out[*k][*i - j] = '\0';
+		(*k)++;
+	}
+	return out;
+}
+
+static char **skip_spaces(char **out, char *str, int *i, int *k)
+{
+	while (str[*i] == ' ')
+		(*i)++;
+	out = handle_token(out, str, i, k);
+	return out;
+}
+
+char **ft_splitpush(char *str)
+{
+	int i = 0;
+	int k = 0;
+	char **out;
 
 	out = malloc((count_w(str) + 2) * sizeof(char *));
 	if (out == NULL)
@@ -54,25 +91,9 @@ char    **ft_splitpush(char *str)
 	k++;
 	while (str[i])
 	{
-		while (str[i] == ' ')
-			i++;
-		j = i;
-		while (str[i] != '\0' && str[i] != ' ')
-			i++;
-		if (i > j)
-		{
-			out[k] = (char *)malloc(sizeof(char) * ((i - j) + 1));
-			if (out[k] == NULL)
-			{  
-				while (k >= 0)
-					free(out[k--]);
-				free(out);
-				return (NULL);
-			}
-			ft_strcopy(out[k], str + j, i - j);
-			out[k][i - j] = '\0';
-			k++;
-		}
+		out = skip_spaces(out, str, &i, &k);
+		if (out == NULL)
+			return (NULL);
 	}
 	out[k] = NULL;
 	return (out);
