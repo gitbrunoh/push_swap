b99bb42bebb598041b0d112256de3f665fd43ddf
Bruno Lopes <brunohenriqueufpr@gmail.com>
2024-02-02 11:54:08 +0000

    resolving conflicts

--- rotate_operation.c
+++ rotate_operation.c
@@ -38,12 +38,6 @@ void	ra(t_stack_node **a)
 	write(1, "ra\n", 3);
 }
 
-void	rb(t_stack_node **b)
-{
-	rotate(b);
-	write(1, "rb\n", 3);
-}
-
 void	rr(t_stack_node **a, t_stack_node **b)
 {
 	rotate(a);
