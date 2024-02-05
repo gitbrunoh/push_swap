639260bdd1c62599405598eda8c26443edff1e44
gitbrunoh <brunohenriqueufpr@gmail.com>
2024-02-05 17:27:27 +0000

    passei a tiny_sort para o ficheito utils2

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: brunhenr <brunhenr@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:47:06 by brunhenr          #+#    #+#             */
-/*   Updated: 2024/01/29 17:47:10 by brunhenr         ###   ########.fr       */
+/*   Updated: 2024/02/05 16:51:18 by brunhenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,17 @@ int	min_n(int a, int b)
 		return (a);
 	else
 		return (b);
+}
+
+void	tiny_sort(t_stack_node **a)
+{
+	t_stack_node	*biggest_node;
+
+	biggest_node = find_max(*a);
+	if (biggest_node == *a)
+		ra(a);
+	else if ((*a)->next == biggest_node)
+		rra(a);
+	if ((*a)->n > (*a)->next->n)
+		sa(a);
 }
